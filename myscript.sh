483dc2b1cadc22e6a3a87fe221bb75e02a8c11df
ExhoQQ <qa.course36@gmail.com>
2023-08-09 15:02:27 +0200

    First HW

---
+++ myscript.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+echo "Helo World"
+echo "Let's start doing my homework"
+echo "My first task was to change directory, that's what we gonna do rn"
+cd /c/QA_Course/Terminal/script/
+mkdir HW1 HW2 HW3 #Создание 3 папок
+cd HW1
+touch test1.txt
+touch test2.txt
+touch test3.txt
+touch test4.json
+touch test5.json
+mkdir name1 name2 name3
+ls
+mv test1.txt name1
+ls
+echo 'That was the end of my script, goodbye and have a nice day!'
